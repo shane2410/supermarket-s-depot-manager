6c261d49da766bff3a88047b085c77fbfb8227bb
thinhanco <tranhuythinh2006@gmail.com>
2025-10-04 21:45:13 +0700

    Update QuanLyNguoiDung.h

--- QL_KhoST/include/QuanLyNguoiDung.h
+++ QL_KhoST/include/QuanLyNguoiDung.h
@@ -1,7 +1,15 @@
 #ifndef QuanLyNguoiDung_h
-#define QuanLyNguoidung_h
-
-class QuanLyNguoiDung{
-
+#define QuanLyNguoiDung_h
+#include <iostream>
+#include "NguoiDung.h"
+#include "LinkedList.h"
+using namespace std;
+class QuanLyNguoiDung : public NguoiDung{
+    LinkedList<NguoiDung>dsnguoidung;
+    public:
+        NguoiDung* nguoidunghientai;
+        NguoiDung dangnhap();
+        NguoiDung dangxuat();
+        NguoiDung thongtinnguoidung();
 };
 #endif
