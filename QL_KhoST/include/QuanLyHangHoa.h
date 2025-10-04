fc16f9b7c471ff238a833d4b1cde57f50d8febde
thinhanco <tranhuythinh2006@gmail.com>
2025-10-04 21:44:56 +0700

    Update QuanLyHangHoa.h

--- QL_KhoST/include/QuanLyHangHoa.h
+++ QL_KhoST/include/QuanLyHangHoa.h
@@ -1,7 +1,19 @@
-#ifndef QuanLyHangHoa_h
-#define QuanLyHangHoa_h
-
-class QuanLyHangHoa{
-
+#ifndef quanlyhanghoa_h
+#define quanlyhanghoa_h
+#include <iostream>
+#include "HangHoa.h"
+#include "LinkedList.h"
+class QuanLyHangHoa : public HangHoa{
+    private: 
+        LinkedList<HangHoa>dshanghoa;
+    public:
+        void themhanghoa();
+        void hienthidanhsach();
+        bool capnhathanghoa();
+        bool xoahanghoa();
+        void timkiemhanghoa();
+        QuanLyHangHoa layhanghoatheoma();
+        void baocaotonkho();
+        void baocaosaphethang();
 };
 #endif
