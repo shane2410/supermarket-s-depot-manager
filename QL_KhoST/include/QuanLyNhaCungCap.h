51e273ea68f5c5fa91dc0b24f0831e996e116758
thinhanco <tranhuythinh2006@gmail.com>
2025-10-04 21:55:24 +0700

    Update QuanLyNhaCungCap.h

--- QL_KhoST/include/QuanLyNhaCungCap.h
+++ QL_KhoST/include/QuanLyNhaCungCap.h
@@ -1,7 +1,16 @@
 #ifndef QuanLyNhaCungCap_h
 #define QuanLyNhaCungCap_h
-
-class QuanLyNhaCungCap{
-
+#include <iostream>
+#include "NhaCungCap.h"
+#include "LinkedList.h"
+using namespace std;
+class QuanLyNhaCungCap : public NhaCungCap{
+    LinkedList <NhaCungCap>dsnhacungcap;
+    public:
+        QuanLyNhaCungCap themnhacungcap();
+        QuanLyNhaCungCap hienthinhacungcap();
+        QuanLyNhaCungCap capnhatnhacungcap();
+        QuanLyNhaCungCap timkiemnhacungcap();
+        QuanLyNhaCungCap kiemtrancc();
 };
 #endif
