c0e4c4a05d2b52125c7cf81ffb21626709ae58e8
thinhanco <tranhuythinh2006@gmail.com>
2025-10-04 21:55:56 +0700

    Update QuanLyPhieuNhap.h

--- QL_KhoST/include/QuanLyPhieuNhap.h
+++ QL_KhoST/include/QuanLyPhieuNhap.h
@@ -1,7 +1,20 @@
-#ifndef QuanLyPhieuNhap_h
-#define QuanLyPhieuNhap_h
-
-class QuanLyPhieuNhap{
-
+#ifndef QuanLyPhieu_h
+#define QuanLyPhieu_h
+#include "Phieu.h"
+#include "LinkedList.h"
+#include "QuanLyHangHoa.h"
+#include "Date.h"
+using namespace std;
+class QuanLyPhieu : public PhieuXuat , public PhieuNhap{
+  LinkedList <PhieuNhap> dsphieunhap;
+  LinkedList <PhieuXuat> dsphieuxuat;
+  public :
+    void taophieunhap();
+    void hienthidsphieunhap();
+    QuanLyPhieu timphieunhap();
+    QuanLyPhieu taophieuxuat(QuanLyHangHoa &qlhh);
+    void hienthidsphieuxuat();
+    void timphieuxuat();
+    void thongkedoanhthu(Date tungay, Date denngay);
 };
 #endif
