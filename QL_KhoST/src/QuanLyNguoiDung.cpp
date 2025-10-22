4cdd79082e981a835a82cf36006392dd90b40b44
thinhanco <tranhuythinh2006@gmail.com>
2025-10-22 22:39:55 +0700

    Update QuanLyNguoiDung.cpp

--- QL_KhoST/src/QuanLyNguoiDung.cpp
+++ QL_KhoST/src/QuanLyNguoiDung.cpp
@@ -1,3 +1,20 @@
 #include "../include/QuanLyNguoiDung.h"
-
-// Your code here
+#include <iostream>
+#include "../include/NguoiDung.h"
+#include <fstream>
+#include <sstream>
+using namespace std;
+NguoiDung QuanLyNguoiDung::dangnhap(const string tk, const string mk){
+    if(tk == getTenDangNhap() && mk == getMatKhau()){
+        cout << "dangnhap tc";
+    }
+}
+void QuanLyNguoiDung::dangxuat() {
+    if (nguoidunghientai.getTenDangNhap().empty()) {
+        cout << "Chua co ai dang nhap!\n";
+    } else {
+        cout << "Dang xuat thanh cong: " << getTenDangNhap() << endl;
+        NguoiDung n;
+       nguoidunghientai = NguoiDung(); 
+    }
+}
